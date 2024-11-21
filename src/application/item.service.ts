6b2b7b86f126f188f77c1980f3797a47bb3f7680
bloomspes <bloomspes@gmail.com>
2024-11-21 11:57:31 +0900

    아이템 추가 기능 작성

--- src/application/item.service.ts
+++ src/application/item.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Item } from '../models/item';
 import { ItemRepository } from '../infrastructure/item.repository';
+import { CreateItemDto } from '../dtos/create-item.dto';
 
 @Injectable()
 export class ItemService {
@@ -18,5 +19,9 @@ export class ItemService {
     }
 
     return item;
+  }
+
+  async createItem(createItemDto: CreateItemDto) {
+    await this.itemRepository.create(createItemDto);
   }
 }
--- src/controllers/item.controller.ts
+++ src/controllers/item.controller.ts
@@ -1,6 +1,17 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
+
 import { ItemService } from '../application/item.service';
+
 import { Item } from '../models/item';
+import { CreateItemDto } from '../dtos/create-item.dto';
 
 @Controller('items')
 export class ItemController {
@@ -14,5 +25,11 @@ export class ItemController {
   @Get(':id')
   async find(@Param('id') id: string): Promise<Item> {
     return await this.itemService.getItem(id);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() createItemDto: CreateItemDto) {
+    await this.itemService.createItem(createItemDto);
   }
 }
